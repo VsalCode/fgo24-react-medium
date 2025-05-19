e3cc1824b245b1c14ca3dfcc30c42735d0dd8ab9
VsalCode <vsal.ilham@gmail.com>
2025-05-19 15:20:01 +0700

    feat: add search feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
+import { IoSearch } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({onSubmit, ...props}) => {
+
   return (
-    <nav className="bg-white fixed top-0 right-0 left-0 flex justify-between px-20 py-5 shadow">
+    <nav className="bg-white fixed top-0 right-0 left-0 flex justify-between items-center px-20 py-5 shadow">
       <div>
         <img className="w-35" src="/src/assets/logo.png" alt="" />
       </div>
+      <form onSubmit={onSubmit} className="flex bg-[#F9F9F9] border px-5 rounded-full w-[700]">
+        <input {...props} className="outline-none grow py-2" type="text" />
+        <button type="submit">
+          <IoSearch/>
+        </button>
+      </form>
       <div className="flex items-center gap-10 font-semibold">
         <Link to="/">Sign In</Link>
         <Link to="/">Sign Up</Link>
